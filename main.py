2ea1b796828edcdf64c0ef1daa9bbfd6b33aeddd
John <98199268+jecko5150@users.noreply.github.com>
2022-12-14 12:46:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+import time
+from turtle import Screen
+
+from scoreboard import Scoreboard, PlayerName
+from snake import Snake
+from food import Food
+
+game_is_on = True
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor('black')
+screen.title("Snakes!!!")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+player = PlayerName()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+screen.onkey(key="1", fun=screen.resetscreen)
+
+
+while game_is_on:
+    screen.update()
+    time.sleep(.1)
+    snake.move()
+
+    #     Detect food with snake
+    if snake.head.distance(food) < 15:
+        scoreboard.increase_score()
+        food.refresh()
+        snake.add_snake()
+
+    #         Detect Wall
+    if snake.head.xcor() > 290:
+        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
+    elif snake.head.ycor() > 290:
+        scoreboard.reset()
+        snake.reset()
+    elif snake.head.xcor() < -290:
+        scoreboard.reset()
+        snake.reset()
+    elif snake.head.ycor() < -290:
+        scoreboard.reset()
+        snake.reset()
+    #     Detect Snake Body
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+screen.mainloop()
